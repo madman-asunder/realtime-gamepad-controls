96baded13810bd27deb674393be4383fbc6a7a52
Adam Sunderman <amsunderman@gmail.com>
2023-01-08 13:10:42 -0600

    updated scripts

---
+++ .githooks/scripts/ensure-utf8-ws-files.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+pushd scripts > /dev/null
+source variables.sh
+
+pushd "$MOD_PATH/src" > /dev/null
+
+for SOURCE_FILE in `find . -iname "*.ws"`; do
+  ENCODING=`file -bi $SOURCE_FILE`
+  ENCODING=${ENCODING##*=}
+  if [ "$ENCODING" == "binary" ]; then 
+    BAD_ENCODING=true
+  fi
+  if [ "$ENCODING" == "utf-16le" ]; then
+    BAD_ENCODING=true
+  fi
+  if [ "$ENCODING" == "utf-16be" ]; then
+    BAD_ENCODING=true
+  fi
+  if [ "$ENCODING" == "utf-16" ]; then
+    BAD_ENCODING=true
+  fi
+done
+
+if [ "$BAD_ENCODING" == true ]; then
+    echo "found ws files with wrong encoding. please run scripts/encoding/encode-ws-utf8.sh. aborting commit..."
+    exit 1
+fi
+
+popd > /dev/null
+popd > /dev/null
